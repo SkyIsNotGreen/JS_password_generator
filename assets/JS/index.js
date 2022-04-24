f4c7f4531b4901c751a6f9ee3039b346dc7527e7
Ricky Palmer <r.palmer@outlook.com>
2022-04-24 03:38:53 +0100

    added password criteria and fixed bad syntax

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -1,25 +1,46 @@
 // Assignment Code
 const generateBtn = document.querySelector("#generate");
 //arrays used to generate PW
-const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
 const special = ["!",'"',"#","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 
 //start of code
 const getPasswordLength = () => {
   //ask user how long password should be
-  const userInput = prompt("Please choose password length between 8 and 128.", "18");
+  const passwordLength = prompt("Please choose password length between 8 and 128.", "18");
   //test if user input is a number and within the correct parameters, if not, discard and stop
-  if (isNaN(userInput) || userInput < 8 || userInput > 128) {
+  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
     alert("Incorrect value, please choose a valid number.");
     return null;
   };
   //if user input is within specified parameters, save in storage
-  return parseInt(userInput);
+  return parseInt(passwordLength);
 };
 
+//password options, array is created based on answers
 const getPasswordCriteria = () => {
+ 
+ const alphaLower = confirm("Would you like your password to include lowercase letters?");
+ const alphaUpper = confirm("Would you like your password to include uppercase letters?");
+ const numbers = confirm("Would you like your password to include numbers?");
+ const special = confirm("Would you like your password to include special characters?");
+ const passwordOptions = [];
+
+ if (alphaLower) {
+   passwordOptions.push(alphaLower);
+ }
+ if (alphaUpper) {
+   passwordOptions.push(alphaUpper);
+ }
+ if (numbers) {
+   passwordOptions.push(numbers);
+ }
+ if (special) {
+   passwordOptions.push(special);
+ }
+ return passwordOptions;
 };
 
 const createRandomPassword = () => {
