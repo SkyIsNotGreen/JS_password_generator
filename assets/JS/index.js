ef23068cf2daf34211b3de97dd8500c0f246004f
Ricky Palmer <r.palmer@outlook.com>
2022-04-24 09:11:17 +0100

    fixed generation to better suit Qs, cleaned syntax

--- assets/JS/index.js
+++ assets/JS/index.js
@@ -4,23 +4,56 @@ const generateBtn = document.querySelector("#generate");
 const alphaLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 const alphaUpper = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
 const numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-const special = ["!",'"',"#","%","&","'","(",")","*","+",",","-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
+const special = ["!", '"', "#", "%", "&", "'", "(", ")", "*", "+", "," ,"-",".","/",":",";","<","=",">","?","@","[","]","^","_","`","{","|","}","~"];
 
-//start of code
+// main function to generate the random password
+const generatePassword = () => {
+  
+
+// get the password length
+const passwordLength = getPasswordLength();
+
+if(passwordLength) {
+// get the password criteria
+  const passwordCriteria = getPasswordCriteria();
+//test if user input is a number and within the correct parameters
+  if(passwordCriteria.length === 0){
+      alert("Password length must be a number between 8-128.")
+
+  }  
+    if(Number.isNaN(passwordLength)) {
+      alert("Incorrect value, please choose a valid number.");
+      }else{
+
+      // create random password
+     const password = createRandomPassword(passwordLength, passwordCriteria);
+      return password;
+      };
+
+      }else{
+          // when user input is incorrect 
+      alert("Please try again.")
+      }
+      };
+
+// get password length
 const getPasswordLength = () => {
   //ask user how long password should be
-  const passwordLength = prompt("Please choose password length between 8 and 128.", "18");
-  //test if user input is a number and within the correct parameters, if not, discard and stop
-  if (isNaN(passwordLength) || passwordLength < 8 || passwordLength > 128) {
-    alert("Incorrect value, please choose a valid number.");
-    location.reload();
-  }
-  //if user input is within specified parameters, save in storage
-  return parseInt(passwordLength);
+  const passLength = prompt("Please choose password length between 8 and 128.", "18");
+
+  const determinePassLength = parseInt (passLength, 10);
+
+  if (determinePassLength >=8 && determinePassLength <= 128){
+} else {
+    alert ("Password length must be a number between 8-128.");
+    return false;
+}
+return determinePassLength;
 };
 
-//password options, array is created based on answers
-const getPasswordOptions = () => {
+
+//password options, array is created based on answers and saved
+const getPasswordCriteria = () => {
  
  const confirmLower = confirm("Would you like your password to include lowercase letters?");
  
@@ -30,36 +63,52 @@ const getPasswordOptions = () => {
  
  const confirmSpecial = confirm("Would you like your password to include special characters?");
  
- const passwordOptions = [];
+ let passwordOptions = [];
 
  if (confirmLower) {
-   passwordOptions.push(alphaLower);
+   passwordOptions = [...alphaLower];
  }
+
  if (confirmUpper) {
-   passwordOptions.push(alphaUpper);
+   passwordOptions = [...passwordOptions, ...alphaUpper];
  }
+
  if (confirmNumbers) {
-   passwordOptions.push(numbers);
+   passwordOptions = [...passwordOptions, ...numbers];
  }
+
  if (confirmSpecial) {
-   passwordOptions.push(special);
+   passwordOptions = [...passwordOptions, ...special];
  }
- return passwordOptions;
+//if you don't select anything, reload page to start again
+ if (
+  !confirmLower &&
+  !confirmUpper &&
+  !confirmNumbers &&
+  !confirmSpecial
+) {
+  alert("At least one of these options must be selected in order to create a password, please try again.");
+  location.reload
+}
+
+return passwordOptions;
 };
 
-
-// main function to generate the random password
-const generatePassword = () => {
-  // get the password length
-  const passwordLength = getPasswordLength();
-
-  // get the password criteria
-  const passwordOptions = getPasswordOptions();
-
-  // create random password
-  const password = createRandomPassword(passwordLength, passwordOptions);
-
-  return password;
+//
+const createRandomPassword = (passwordLength, passwordOptions) => {
+  const passwordArray =[];
+  for(let i = 0; i < passwordLength; i+=1){
+  // select random categories from the array
+  const randomCategoryElement = Math.floor(Math.random() * passwordOptions.length);
+  // getting random categories 
+  const randomCategory = passwordOptions[randomCategoryElement];
+  // getting random element
+  const randomElement = Math.floor(Math.random() * randomCategory.length);
+  // getting random character
+  const randomCharacter = randomCategory.charAt(randomElement)
+  passwordArray.push(randomCharacter);
+  }
+  return passwordArray.join("");
 };
 
 // Write password to the #password input
